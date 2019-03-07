2283ae7b4433486f1051a57dfc28e899b6a2ff60
visualstudio <visualstudio@230-PC14>
2019-03-07 14:32:04 -0500

    C0709562_Sukhraj_singh

--- C0709562/Program.cs
+++ C0709562/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace C0709562
+namespace C0709562_Assignment2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Student Name Sukhraj singh Singh Student Id C0709562
+            //CSD3354 Section 2 
+            //Assignmnet 2
+            //March 7,2019
+            DelegateExcercises a = new DelegateExcercises();
+            a.Method2();
         }
     }
+
+
+    public class DelegateExcercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+
 }
+
